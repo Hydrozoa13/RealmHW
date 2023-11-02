7a8b89640866ce76e9a35d0a4ed2211b77d0279c
Zhenya Loyko <e.lojko@inbox.ru>
2023-11-02 09:34:00 +0300

    Project created

---
+++ RealmHW/RealmHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmHW
+//
+//  Created by Евгений Лойко on 2.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
